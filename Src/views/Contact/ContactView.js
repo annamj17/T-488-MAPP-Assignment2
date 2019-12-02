88381a1c596382b13d02d5598963772cf00db164
Arnar <arnara17@ru.is>
2019-12-02 14:16:31 +0000

    Data added

--- Src/views/Contact/ContactView.js
+++ Src/views/Contact/ContactView.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { withNavigation } from 'react-navigation';
-
+import { getAllImages, addImage } from '../../services/services';
 import data from '../../resources/data';
 import RenderAllContacts from '../../components/RenderAllContacts/RendarAllContacts';
 
@@ -9,35 +9,43 @@ const contacts = data.boards;
 
 class ContactView extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            id: '',
-            data: contacts,
-            name: '',
-            thumbnailPhoto: '',
-            text: '',
-        }
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: '',
+			data: contacts,
+			contacts: [],
+		}
+	};
 
-    render() {
-        const { navigate } = this.props.navigation;
-        return (
-            <View style={styles.screens}>
-                <RenderAllContacts
-                    contacts={this.state.data}
-                    onPress={id => navigate('ContactDetailView', { id: id })}
-                />
+	async componentDidMount() {
+		await this._fetchItems();
+	}
 
-            </View>
-        );
-    }
+	async _fetchItems() {
+		const contacts = await getAllImages();
+		console.log(contacts);
+		this.setState({ contacts });
+	}
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View style={styles.screens}>
+				<RenderAllContacts
+					contacts={this.state.contacts}
+					onPress={id => navigate('ContactDetailView', { id: id })}
+
+				/>
+
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  screens: {
-    flex: 1
-  }
+	screens: {
+		flex: 1
+	}
 });
 
 export default ContactView;

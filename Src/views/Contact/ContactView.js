5e327b70d3151b1ede4369d9f35f5a3f41e53158
annamj <annamj17@ru.is>
2019-12-02 12:20:37 +0000

    setting up routes

---
+++ Src/views/Contact/ContactView.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { withNavigation } from 'react-navigation';
+
+import data from '../../resources/data';
+import RenderAllContacts from '../../components/RenderAllContacts/RendarAllContacts';
+
+const contacts = data.boards;
+
+class ContactView extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            data: contacts,
+            name: '',
+            thumbnailPhoto: '',
+            text: '',
+        }
+    };
+
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.screens}>
+                <RenderAllContacts
+                    contacts={this.state.data}
+                    onPress={id => navigate('ListView', { id: id })}
+                />
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  screens: {
+    flex: 1
+  }
+});
+
+export default ContactView;
